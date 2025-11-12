7fb3e1d528e2b6fe6836754562a8e20b3248c968
Ethan <ethangarcia0927@gmail.com>
2025-11-12 15:38:12 -0800

    more

--- cst336/lab3/js/script.js
+++ cst336/lab3/js/script.js
@@ -1,16 +1,50 @@
 // event listeners
 document.querySelector("#zip").addEventListener("change",displayCity);
+document.querySelector("#state").addEventListener("change",displayCounties);
+document.querySelector("#username").addEventListener("change",checkUsername);
 
 // functions
 
 // Displaying city from Web API after entering a zip code
 async function displayCity() {
     //alert(document.querySelector("#zip").value);
-    let zipCode = document.querySelector("#zip").ariaValueMax;
+    let zipCode = document.querySelector("#zip").value;
     //console.log(zipCode);
     let url = `https://csumb.space/api/cityInfoAPI.php?zip=${zipCode}`;
     let response = await fetch(url);
     let data = await response.json();
     // console.log(data);
     document.querySelector("#city").innerHTML = data.city;
+    document.querySelector("#latitude").innerHTML = data.latitude;
+    document.querySelector("#longitude").innerHTML = data.longitude;
+}
+
+// Displaying counties from Web API based on the two-letter abbreviation of a state
+async function displayCounties() {
+    let state = document.querySelector("#state").value;
+    let url = `https://csumb.space/api/countyListAPI.php?state=${state}`;
+    let response = await fetch(url);
+    let data = await response.json();
+    let countyList = document.querySelector("#county");
+    countyList.innerHTML = "<option> Select County </option>";
+    for (let i=0; i < data.length; i++) {
+        countyList.innerHTML += `<option> ${data[i].county} </option>`;
+    }
+}
+
+// Checking whether the username is available
+async function checkUsername() {
+    let username = document.querySelector("#username").value;
+    let url = `https://csumb.space/api/usernamesAPI.php?username=${username}`;
+    let response = await fetch(url);
+    let data = await response.json();
+    let usernameError = document.querySelector("#usernameError")
+    if (data.available) {
+        usernameError.innerHTML = " Username available!";
+        usernameError.style.color = "green";
+    }
+    else {
+        usernameError.innerHTML = " Username taken";
+        usernameError.style.color = "red";
+    }
 }
