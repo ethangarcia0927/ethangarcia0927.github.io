6c2633704c83a6ca9717e6f16a16b83c3ac94732
Ethan <ethangarcia0927@gmail.com>
2025-11-12 16:44:56 -0800

    bootstrapped

--- cst336/lab3/js/script.js
+++ cst336/lab3/js/script.js
@@ -5,6 +5,7 @@ document.querySelector("#username").addEventListener("change",checkUsername);
 document.querySelector("#signupForm").addEventListener("submit", function(event) {
     validateForm(event);
 });
+document.querySelector("#password").addEventListener("focus", suggestPassword);
 
 // functions
 
@@ -12,14 +13,21 @@ document.querySelector("#signupForm").addEventListener("submit", function(event)
 async function displayCity() {
     //alert(document.querySelector("#zip").value);
     let zipCode = document.querySelector("#zip").value;
-    //console.log(zipCode);
+
     let url = `https://csumb.space/api/cityInfoAPI.php?zip=${zipCode}`;
     let response = await fetch(url);
     let data = await response.json();
-    // console.log(data);
-    document.querySelector("#city").innerHTML = data.city;
+
+    if (!data || !data.city) {
+    document.querySelector("#city").innerHTML = "Zip code not found";
+    document.querySelector("#latitude").innerHTML = "";
+    document.querySelector("#longitude").innerHTML = "";
+    } else {
+     document.querySelector("#city").innerHTML = data.city;
     document.querySelector("#latitude").innerHTML = data.latitude;
     document.querySelector("#longitude").innerHTML = data.longitude;
+    }
+    
 }
 
 // Displaying counties from Web API based on the two-letter abbreviation of a state
@@ -57,8 +65,15 @@ function validateForm(e) {
     let isValid = true;
     let username = document.querySelector("#username").value;
     let password = document.querySelector("#password").value;
+    let retypePassword = document.querySelector("#retypePassword").value;
+
     let usernameError = document.querySelector("#usernameError");
     let passwordError = document.querySelector("#passwordError");
+
+    // Clear previous errors
+    usernameError.innerHTML = "";
+    passwordError.innerHTML = "";
+
 
     // Username checks
     if (username.length === 0) {
@@ -71,14 +86,42 @@ function validateForm(e) {
 
     // Password Checks
     if (password.length < 6) {
-        passwordError.innerHTML = "Pasword must be at least six characters!";
+        passwordError.innerHTML = "Password must be at least six characters!";
         isValid = false;
-    } else if (password !== retypePassword) {
+    } 
+    else if (password !== retypePassword) {
         passwordError.innerHTML = "Passwords do not match!";
         isValid = false;
     }
     
     if (!isValid) {
         e.preventDefault();
+    }
+}
+
+// Suggest password generator
+// Source: https://stackoverflow.com/questions/1497481/javascript-password-generator
+function suggestPassword() {
+    let suggestedPwd = document.querySelector("#suggestedPwd");
+    let charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789",
+        password = "";
+    for (var i = 0, n = charset.length; i < 9; ++i) {
+        password += charset.charAt(Math.floor(Math.random() * n));
+    }
+    suggestedPwd.innerHTML = `Try this: ${password}`;
+    suggestedPwd.style.color = "blue";
+}
+
+window.onload = displayStates;
+async function displayStates() {
+    let url = "https://csumb.space/api/allStatesAPI.php";
+    let response = await fetch(url);
+    let data = await response.json();
+
+    let stateDropdown = document.querySelector("#state");
+    stateDropdown.innerHTML = "<option>Select One</option>";
+
+    for (let state of data) {
+        stateDropdown.innerHTML += `<option value="${state.usps}">${state.state}</option>`;
     }
 }
